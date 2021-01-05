99ac9fcbe513aab22ed87c094a17478b843ee3a1
Tiago Batista <7.tiagobatista@gmail.com>
2021-01-05 05:11:35 -0300

    Instalaçao da estrutura MVC e criacao do CategoriaController

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require 'config/config.php';
+require 'app/core/Core.php';
+require 'vendor/autoload.php';
+date_default_timezone_set("America/Fortaleza");
+
+$core = new Core;
+$core->run();
+
+/*
+echo "contoller: " .$core->getController();
+echo "<br>Método : " .$core->getMetodo();
+$parametros = $core->getParametros();
+foreach ($parametros as $param)
+    echo "<br>Parâmetro : " .$param;*/
+
